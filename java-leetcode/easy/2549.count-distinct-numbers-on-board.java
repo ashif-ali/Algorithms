0bf415e7811cebf89c637846a805228109fde28b
Sk Md Ashif Ali <36637666+ashif-ali@users.noreply.github.com>
2025-08-06 20:04:45 +0530

    Count Distinct Numbers on Board java

---
+++ java-leetcode/easy/2549.count-distinct-numbers-on-board.java
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode id=2549 lang=java
+ *
+ * [2549] Count Distinct Numbers on Board
+ */
+
+// @lc code=start
+class Solution {
+    public int distinctIntegers(int n) {
+
+        if (n == 1)
+            return 1;
+        return n - 1;
+
+    }
+}
+// @lc code=end
