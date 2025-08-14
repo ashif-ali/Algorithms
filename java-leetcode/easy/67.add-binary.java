e5593a46c8855e00ca562b927a8902b9c4826488
Sk Md Ashif Ali <36637666+ashif-ali@users.noreply.github.com>
2025-08-14 19:26:21 +0530

    Add Binary java

---
+++ java-leetcode/easy/67.add-binary.java
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode id=67 lang=java
+ *
+ * [67] Add Binary
+ */
+
+// @lc code=start
+class Solution {
+    public String addBinary(String a, String b) {
+        // Initialize a StringBuilder to store the result
+        StringBuilder result = new StringBuilder();
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+        int carry = 0;
+        while (i >= 0 || j >= 0 || carry > 0) {
+            int sum = carry;
+            if (i >= 0) {
+                sum += a.charAt(i) - '0';
+                i--;
+
+            }
+            if (j >= 0) {
+                sum += b.charAt(j) - '0';
+                j--;
+            }
+            result.append(sum % 2);
+            carry = sum / 2;
+        }
+        return result.reverse().toString();
+    }
+}
+// @lc code=end
