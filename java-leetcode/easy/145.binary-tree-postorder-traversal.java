d089e82ebaf81e808b72f694794f21cf7d58b71b
Sk Md Ashif Ali <36637666+ashif-ali@users.noreply.github.com>
2025-08-15 20:35:55 +0530

    postorder traversal java

---
+++ java-leetcode/easy/145.binary-tree-postorder-traversal.java
@@ -0,0 +1,42 @@
+/*
+ * @lc app=leetcode id=145 lang=java
+ *
+ * [145] Binary Tree Postorder Traversal
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        // Initialize a list to store the result
+        List<Integer> result = new ArrayList<>();
+        // Call the helper function to perform postorder traversal
+        postorderHelper(root, result);
+        return result;
+    }
+
+    private void postorderHelper(TreeNode node, List<Integer> result) {
+        if (node == null) {
+            return;
+        }
+        postorderHelper(node.left, result);
+        postorderHelper(node.right, result);
+        result.add(node.val);
+    }
+
+}
+// @lc code=end
